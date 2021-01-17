5021ed91993754c3cc4289dfbd1d89fcc4187b6c
yehudis raitman <yehudisr@gmail.com>
2021-01-17 18:07:27 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,5 +21,13 @@ def output_array(array)
   count += 1 
 end
 end
+
+def return_string_array(array)
+  count = 0 
+  while count < array.length 
+  return array[count].to_s
+  count += 1 
+end
+end
   
   
