5bb5c6b16c2f8da9fd2b2ce3a5497c367a700637
yehudis raitman <yehudisr@gmail.com>
2021-01-17 18:23:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,6 @@
 def loop_message_five_times(string)
   count = 0
-  while count < 5
+  while count < 5 do
   puts string
   count += 1 
 end
@@ -8,7 +8,7 @@ end
 
 def loop_message_n_times(string, integer)
   count = 0 
-  while count < integer
+  while count < integer do
   puts string
   count += 1 
 end
@@ -16,7 +16,7 @@ end
   
 def output_array(array)
   count = 0 
-  while count < array.length 
+  while count < array.length do
   puts array[count]
   count += 1 
 end
@@ -24,10 +24,12 @@ end
 
 def return_string_array(array)
   count = 0 
-  while count < array.length 
-  return array[count].to_s
+  new_array = []
+  while count < array.length do
+  new_array.push(array[count].to_s)
   count += 1 
 end
+new_array
 end
   
   
